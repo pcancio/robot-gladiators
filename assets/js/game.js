c4f3747e6d59389a6d480f06e284f9abf7697213
pcancio <pcancio@gmail.com>
2021-10-20 23:08:15 -0400

    step 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,38 @@
-function fight() {window.alert("The flight has begun!");}
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");}
+var playerAttack = 10;
+if (playerHealth === 0) {
+    console.log("This will not run.");
+}
+else {
+    console.log("This will run instead.");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+var fight = function() {
+    window.alert("welcome to Robot Gladiators");
+};
+enemyHealth = enemyHealth - playerAttack;
+console.log(
+playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+if (enemyHealth <= 0) {
+    window.alert (enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has" + enemyHealth + " health left.");
+}
+
+playerHealth = playerHealth - enemyAttack;
+console.log(
+    enemyName + " atacked " + playerName + " . " + playerName + " now has " + playerHealth + "health remaining.");
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + "health left.");
+}
+
+}
 fight();
-window.prompt("what is your robot's name?");
-var playerName = window.prompt("Tony the Robot");
-//fight();
-var playerName = window.prompt("What is your robot's name");
-//What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10 +10);
-//what is this?
-console.log("Our robot's name is + playerName");
